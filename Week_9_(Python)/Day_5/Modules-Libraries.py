0f417dae6ffc32e314ccd6bcf3710749e927b423
dkmda4 <deonte.da@outlook.com>
2023-03-10 14:30:04 +0000

    Work thats been renamed and  also new files

---
+++ Week_9_(Python)/Day_5/Modules-Libraries.py
@@ -0,0 +1,28 @@
+# Modules/Libraries
+# Modules are reuseable code files/scripts.
+
+from random import random
+
+listA = [10, 20, 30]
+
+"""
+Examples of innate functions
+
+listA.pop()
+listA.len()
+print()
+
+"""
+
+# Generating a random number
+# we need to import a random, from the python standard library
+# Random() will generate a random float between 0 and 1
+
+x = random()
+print(x)
+
+x = random()
+print(x)
+
+x = random()
+print(x)
